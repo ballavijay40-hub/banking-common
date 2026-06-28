1ea05687516cb36952e2bc10dd20cffa2e3cecd0
Balla vijay kumar <ballavijay40@gmail.com>
2026-06-28 09:07:34 +0530

    added some comon event

---
+++ src/main/java/com/banking/microservices/common/constants/KafkaTopics.java
@@ -0,0 +1,11 @@
+package com.banking.microservices.common.constants;
+
+public  final  class   KafkaTopics {
+    private KafkaTopics() {
+
+    }
+
+    // Constants
+    public static final String TRANSACTION_REQUESTS = "transaction-requests";
+    public static final String TRANSACTION_RESULTS = "transaction-results";
+}
---
+++ src/main/java/com/banking/microservices/common/enums/TransactionStatus.java
@@ -0,0 +1,11 @@
+package com.banking.microservices.common.enums;
+
+
+
+public enum TransactionStatus {
+        SUCCESS,
+        PENDING,
+        FAILED
+    }
+
+
